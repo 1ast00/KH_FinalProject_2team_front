844bf8fc0a14111c8cc2ae994a64561f8bae04af
1ast00 <worud3017@naver.com>
2025-08-25 14:53:12 +0900

    메인, 추천 운동

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from "./components/Footer";
 import MainPage from "./pages/MainPage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import ExerciseListPage from "./pages/ExerciseListPage";
+import ExerciseDetailPage from "./pages/ExerciseDetailPage";
 
 function App() {
   return (
@@ -16,7 +18,12 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
+
+          {/* 추천 운동 페이지 라우트 */}
+          <Route path="/exercise" element={<ExerciseListPage />} />
+          <Route path="/exercise/:exerciseName" element={<ExerciseDetailPage />} />
           {/* 비공개 라우트 */}
+          
           <Route />
           {/* 404방지 */}
           <Route path="*" element={<h2>404</h2>} />
