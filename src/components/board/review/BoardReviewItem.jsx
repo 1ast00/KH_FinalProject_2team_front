90cde14dcaecb175a327326fa50a3a135762f7a4
scis <scis@kakao.com>
2025-09-04 13:57:55 +0900

    글1개 출력

--- src/components/board/review/BoardReviewItem.jsx
+++ src/components/board/review/BoardReviewItem.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "../../../css/board/BoardReviewAllview.module.css"; // CSS 파일 경로 수정
+import styles from "../../../css/board/BoardReviewAllview.module.css";
 
 export default ({ review, displayNo }) => {
-  // 컴포넌트가 <tr>을 반환하도록 수정합니다.
   return (
-    <tr className={styles.boardRow}>
-      <td className={styles.column_brno}>{displayNo}</td>
-      <td className={styles.column_brtitle}>
-        {/* to 속성은 프런트엔드 라우팅 경로를 의미 */}
+    <>
+      <td className={styles.col_brno}>{displayNo}</td>
+      <td className={styles.col_brtitle}>
         <Link to={`/board/review/${review.brno}`}>{review.brtitle}</Link>
       </td>
-      <td className={styles.column_mname}>{review.mname}</td>
-      <td className={styles.column_date}>
+      <td className={styles.col_nickname}>{review.nickname}</td>
+      <td className={styles.col_date}>
         {new Date(review.brwrite_date).toLocaleDateString()}
       </td>
-      <td className={styles.column_viewcount}>{review.brviewcount}</td>
-      <td className={styles.column_likes}>❤️</td>
-    </tr>
+      <td className={styles.col_viewcount}>{review.brviewcount}</td>
+      <td className={styles.col_likes}>❤️</td>
+    </>
   );
 };
