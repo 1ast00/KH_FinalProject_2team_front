86223f50d12d97514063aa6bbef9e78a3987b825
scis <scis@kakao.com>
2025-09-09 18:51:18 +0900

    주간mvp

--- src/service/authApi.js
+++ src/service/authApi.js
@@ -10,7 +10,7 @@ import {
 // 백엔드 서버 주소
 const API_BASE_URL = "http://localhost:9999/api";
 
-const authApi = axios.create({
+export const authApi = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true, // CORS 요청 시 쿠키나 HTTP 인증 헤더를 포함할지 여부
 });
